0bfd803058263ce107eb1463a8ccb8bbc9c57c48
Ali-Key <mafiobio700@gmail.com>
2023-06-20 06:30:54 -0700

    last updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
-
 	/**
 	 * print_alphabet - print all alphabet in lowercase
 	 */
-
 
 	void print_alphabet(void)
 	{
@@ -13,7 +11,6 @@
 
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-
 
 		_putchar('\n');
 	}
